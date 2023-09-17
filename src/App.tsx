b437f9206998d21d540afe6a10d79472a3a93d4d
mirza-sync <mirzasyahmi123@gmail.com>
2023-09-17 23:15:33 +0800

    Feat: Player & computer take turns

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Board from "./components/Board";
 import Square from "./components/Square";
@@ -6,10 +6,24 @@ import Square from "./components/Square";
 function App() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
+  useEffect(() => {
+    const isComputerTurn =
+      squares.filter((square) => square !== null).length % 2 !== 0;
+
+    if (isComputerTurn) {
+      alert("Computer");
+    }
+  }, [squares]);
+
   function handleSquareClick(index: number) {
-    let newSquares = squares;
-    newSquares[index] = "X";
-    setSquares([...newSquares]);
+    const isPlayerTurn =
+      squares.filter((square) => square !== null).length % 2 === 0;
+
+    if (isPlayerTurn) {
+      let newSquares = squares;
+      newSquares[index] = "X";
+      setSquares([...newSquares]);
+    }
   }
 
   return (
